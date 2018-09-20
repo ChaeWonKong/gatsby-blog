49db1f937542339b126e98b0580a9e9e55fc8840
공채원 <chaewonkong@gmail.com>
2018-09-20 12:03:19 +0900

    Created about.js page

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,34 @@
 import React from "react";
 import "./header.modules.css";
+import { Link } from "gatsby";
 
 export default () => (
   <header className="header">
-    <h1>leonkong.com</h1>
+    <h1>
+      <Link to="/" className="header-link">
+        leonkong.com
+      </Link>
+    </h1>
+
     <div className="header-right">
       <ul className="header-list">
-        <li>about</li>
-        <li>devlog</li>
-        <li>projects</li>
+        <li>
+          <Link to="/about/" className="header-link">
+            about
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/" className="header-link">
+            devlog
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/" className="header-link">
+            projects
+          </Link>
+        </li>
       </ul>
       <div className="header-search">
         <input className="header-input" placeholder="search" type="text" />
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../components/header";
+
+export default () => (
+  <div>
+    <Header />
+    <p>It's all about me</p>
+  </div>
+);
