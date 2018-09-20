c4615d5c4f219fe0790b7858e23e9555e6d7e9de
공채원 <chaewonkong@gmail.com>
2018-09-20 14:02:00 +0900

    Add lorem50 to about.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,17 +6,7 @@ export default ({ children }) => (
   <div>
     <Header />
     <div className="container">
-      <div className="post">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit,
-          aliquid! Culpa aperiam maiores ipsam fugit eos quae debitis, ex unde
-          hic vero! Placeat accusantium dignissimos optio quia delectus et
-          cupiditate unde odit veniam maiores. Quam consequuntur provident
-          accusamus deserunt qui sapiente amet veniam velit illo possimus! Ipsum
-          asperiores dolorum voluptatibus!
-        </p>
-        {children}
-      </div>
+      <div className="post">{children}</div>
     </div>
   </div>
 );
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,14 @@ export default () => (
   <Layout>
     <div>
       <p>It's all about me</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit,
+        aliquid! Culpa aperiam maiores ipsam fugit eos quae debitis, ex unde hic
+        vero! Placeat accusantium dignissimos optio quia delectus et cupiditate
+        unde odit veniam maiores. Quam consequuntur provident accusamus deserunt
+        qui sapiente amet veniam velit illo possimus! Ipsum asperiores dolorum
+        voluptatibus!
+      </p>
     </div>
   </Layout>
 );
