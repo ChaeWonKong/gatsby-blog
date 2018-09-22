dd26dbeb1db0ea2ce7b523ec60ef44dfdbd1c877
공채원 <chaewonkong@gmail.com>
2018-09-22 20:00:13 +0900

    Change in blog-post.modules.css

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Layout from "../components/layout";
+import "./blog-post.modules.css";
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <Layout>
       <div>
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <h1 className="post-h1">{post.frontmatter.title}</h1>
+        <div
+          className="blog-post"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
       </div>
     </Layout>
   );
